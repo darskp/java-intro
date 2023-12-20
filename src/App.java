8b2d0ea952c16b7aee38220baad5081ca6515943
darskp <darshanpatgar100@gmail.com>
2023-12-20 15:21:49 +0530

    java intro

---
+++ src/App.java
@@ -0,0 +1,44 @@
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+
+// public class App{
+// public static void main(String[] args){
+//     String name="Darshan";
+//     int a=10;
+
+// System.out.println("Check the concat");
+// }
+// }
+
+
+// public class App {
+
+//     public static void main(String[] args) {
+//         int a = 5;
+//  int b = 10;
+//  int sum = a+b;
+//  System.out.println("Sum: "+sum);//15
+//  System.out.println(a-2);//3
+//  System.out.println(4*b);//40
+//  System.out.println(10/5);//2
+//  System.out.println(10%2); //0
+//     }
+// }
+
+// public class App {
+
+//     public static void main(String[] args) {
+//         System.out.println("hai");
+//  System.out.print("hai");
+//  System.out.print(" bye"+10);
+//  System.out.println("java");
+//  System.out.print("hai"+" hello");
+//  System.out.print("hai");
+//  System.out.println("bye");
+//     }
+// }
+
